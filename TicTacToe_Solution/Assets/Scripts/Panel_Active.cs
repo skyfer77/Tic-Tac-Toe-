b23af53b88e5a3e6de70e9ab2d2b73637101afe0
Сергей Безпалый < >
2017-04-07 01:32:24 +0300

    Solition

---
+++ TicTacToe_Solution/Assets/Scripts/Panel_Active.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_Active : MonoBehaviour {
+	public GameObject panel;
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.GetString ("Difficult") == "Human") {
+			panel.SetActive (false);
+		}
+		else 
+			panel.SetActive (true);
+	}
+	
+
+}
